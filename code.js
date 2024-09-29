ead67f46ec80dcdcaaea00cb00e567b856dfa925
Nile Young <nilevy03@gmail.com>
2024-09-28 19:58:07 -0600

    implemented flip

--- code.js
+++ code.js
@@ -1,8 +1,24 @@
-function flip(array, n) {
+function flip (array, n) {
+  let i = 0;
+  let mid = Math.floor(n / 2);
+
+  if (n >= array.length) {
+    array.reverse();
     return array;
+  }
+
+  for (i; i < mid; i++) {
+    let tmp = array[i];
+    array[i] = array[n-1];
+    array[n-1] = tmp;
+  }
+
+  return array;
 }
 
 // Use only flip() here to manipulate the array
-function pancakeSort(array) {
-    return array;
+function pancakeSort (array) {
+    let mi = array.length;
+    
+    return array
 }
